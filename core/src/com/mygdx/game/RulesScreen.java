14624077850e15d472fb5a35070acf55441d8c43
ChrisChin <chinchri1@myvuw.ac.nz>
2016-02-08 12:27:56 +1300

    created informationscreen to remove duplicate code
    Created the InformationScreen abstract class to remove duplicate code in
    the about and rules screens

--- core/src/com/mygdx/game/RulesScreen.java
+++ core/src/com/mygdx/game/RulesScreen.java
@@ -3,80 +3,24 @@ package com.mygdx.game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.files.FileHandle;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.InputListener;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Button;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-
-public class RulesScreen implements Screen {
-    private Main main;
-    private String message;
-    private final int width = Gdx.graphics.getWidth();
-    private final int height = Gdx.graphics.getHeight();
-    private Stage stage;
-    private Button button;
+public class RulesScreen extends InformationScreen implements Screen {
 
     public RulesScreen(Main main){
-        this.main = main;
-        stage = new Stage();
-        stage.clear();
+        super(main);
+        setHeader("BlackJack Rules");
     }
 
     @Override
     public void show() {
-        Gdx.input.setInputProcessor(stage);
-        button = new MyButton("arrow");
-        button.setPosition(width - button.getWidth(), height - button.getHeight());
-        button.addListener(new InputListener() {
-            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                return true;
-            }
-            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-                main.setScreenMainMenu();
-            }
-        });
-        stage.addActor(button);
+        super.show();
         FileHandle file = Gdx.files.internal("data/Rules.txt");
-        message = file.readString();
+        setMessage(file.readString());
     }
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(0.1f, 0.1f, 0.1f, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        BitmapFont font = new BitmapFont();
-        SpriteBatch spriteBatch = new SpriteBatch();
-        stage.act();
-        spriteBatch.begin();
-        stage.draw();
-        spriteBatch.end();
-        spriteBatch.begin();
-
-        //header
-        float x = width/20;
-        float fontWidth = button.getX() - x;
-        String header = "BlackJack Rules";
-        font.setColor(Color.WHITE);
-        font.setScale(button.getHeight()/32);
-        font.drawWrapped(spriteBatch,header,x,height*19/20 ,fontWidth);
-
-        //rules
-        float y = button.getY()- height/20;
-        font.setScale(font.getScaleY()/2);
-        font.drawWrapped(spriteBatch,message,x, y,width*18/20);
-
-        spriteBatch.end();
-        font.dispose();
-        spriteBatch.dispose();
+       super.render(delta);
     }
 
     @Override
